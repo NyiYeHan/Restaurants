85cf50d21fe9604e53121b9849d41574497bab20
Nyi Ye Han <nyiyehan@gmail.com>
2023-10-24 09:40:38 +0630

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Barry on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
